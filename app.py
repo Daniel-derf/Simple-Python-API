4101c09924947a006ebfbe147044aae88466269e
Daniel <danielpardinho321@gmail.com>
2023-09-28 23:09:56 -0300

    create book function

--- app.py
+++ app.py
@@ -41,5 +41,12 @@ def edit_book_by_id(id):
             return jsonify(books[index])
 
 
+@app.route('/books', methods=['POST'])
+def create_new_book():
+    new_book = request.get_json()
+    books.append(new_book)
+
+    return jsonify(books)
+    
 
 app.run(port=5000, host='localhost', debug=True)
