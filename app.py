a47244cfaa93f222297422661b4a99df7d51ff17
Daniel <danielpardinho321@gmail.com>
2023-09-28 23:06:49 -0300

    edit books functions

--- app.py
+++ app.py
@@ -24,11 +24,21 @@ books = [
 def get_books():
     return jsonify(books)
 
+
 @app.route('/books/<int:id>', methods=['GET'])
 def get_book_by_id(id):
     for book in books:
         if book.get('id') == id:
             return jsonify(book)
+
+
+@app.route('/books/<int:id>', methods=['PUT'])
+def edit_book_by_id(id):
+    edited_book = request.get_json()
+    for index, book in enumerate(books):
+        if book.get('id') == id:
+            books[index].update(edited_book)
+            return jsonify(books[index])
 
 
 
